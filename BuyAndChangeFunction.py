c017d875c2a3fa1bca5d240c8d8c268cc2cdbb92
SHIN_LIM <shinilim@iskolarngbayan.pup.edu.ph>
2021-11-08 23:13:49 +0800

    sixth commit

--- BuyAndChangeFunction.py
+++ BuyAndChangeFunction.py
@@ -5,9 +5,6 @@ def welcomeUser():
 def display(nickname_):
     print(f"Good day {nickname_}!. Please enjoy your shopping.")
 
-nickname = welcomeUser()
-display(nickname)
-
 def askMoneyAndApple():
     global money
     money = int(input("How much money do you have on hand?: "))
@@ -23,8 +20,10 @@ def applesAndChange():
     return numOfApple_, change_
 
 def displayOutput(numOfApple_, change_):
-    print(f"You can buy {numOfApple_} apples and your change is {change_} pesos.")
+    print(f"You can buy {numOfApple_} apple/s and your change is {change_} pesos.")
 
+nickname = welcomeUser()
+display(nickname)
 money, priceOfApple = askMoneyAndApple()
 numOfApple_, change_ = applesAndChange()
 displayOutput(numOfApple_, change_)
